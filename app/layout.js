d7c5b383930a351e39c5f3eac39b606f1f3e9481
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-30 19:58:04 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+title: 'Fashion Nova',
+description: 'Fashion Nova Ecommerce',
+}
+
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/light.min.css" />
+</head>
+<body className={inter.className}>
+{children}
+</body>
+</html>
+)
+}
