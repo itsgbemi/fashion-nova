a49acb0eb78b64d3e29066d168f39415b363fa18
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-30 18:47:37 +0100

    Create page.js

---
+++ app/components/page.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+export default function Home(){
+return(
+<main>
+<Header/>
+<Hero/>
+<Footer/>
+</main>
+)
+}
