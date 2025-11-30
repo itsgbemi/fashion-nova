eb55c7a8605b686daf699cbbe52e1ff7dad6c2d4
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-30 19:06:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-experimental:{
-appDir:true,
-},
-images:{
-domains:['res.cloudinary.com'],
+images: {
+domains: ['res.cloudinary.com'],
 },
 }
-module.exports=nextConfig
+
+module.exports = nextConfig
